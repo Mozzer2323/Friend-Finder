57e4f5e4d621fef5aa305369384ddfd30c7b3153
Lowden Hall <53575153+Mozzer2323@users.noreply.github.com>
2019-12-03 19:05:51 -0600

    Completed

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var friends = require('./app/data/friends.js');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static('app/public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
+app.use(express.json({ type: 'application/vnd.api+json' }));
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(process.env.PORT || 3000);
